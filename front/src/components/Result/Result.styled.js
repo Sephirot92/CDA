2e447057d911354ca38522fac3276523ccb64e31
WojciechWolak <wojciech.wolak@ace-agency.co>
2023-06-03 04:30:30 +0200

    Fix: bug with percentag value in cell

--- front/src/components/Result/Result.styled.js
+++ front/src/components/Result/Result.styled.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 40px;
   padding: 96px 0;
-  max-width: 940px;
+  max-width: 1040px;
   margin: 0 auto;
 `;
 
 const Header = styled.header`
   display: flex;
+  width: 100%;
   justify-content: space-evenly;
   align-items: center;
 `;
@@ -26,6 +28,7 @@ const Title = styled.h1`
 const List = styled.ul`
   list-style: none;
   display: flex;
+  padding: 0;
   align-items: stretch;
   gap: 16px;
   font-weight: 300;
